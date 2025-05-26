b9ff56c636062a2262b3c8008ffeee917dfe1b21
Victor Soares <victorsoaresds.contato@gmail.com>
2025-05-26 10:59:18 -0300

    Criando um .bat de start do projeto e ajustando rotas

--- FrontEnd/src/api/ListaApi.jsx
+++ FrontEnd/src/api/ListaApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:7014/",
+    baseURL: "http://localhost:7014/",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
